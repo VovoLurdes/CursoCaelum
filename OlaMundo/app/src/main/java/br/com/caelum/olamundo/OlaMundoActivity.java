aa52507ce32b2815224c177fad8703193610b639
VovoLurdes <flaviog.info@gmail.com>
2015-07-21 19:46:47 -0300

    Segunda Aula

---
+++ OlaMundo/app/src/main/java/br/com/caelum/olamundo/OlaMundoActivity.java
@@ -0,0 +1,16 @@
+package br.com.caelum.olamundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by android5195 on 20/07/15.
+ */
+public class OlaMundoActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ola_mundo);
+    }
+
+}
